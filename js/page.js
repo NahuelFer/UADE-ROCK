7980ea2b5df3b0ee64f68b0a1660a0d93ca11d79
NahuelFer <102939580+NahuelFer@users.noreply.github.com>
2024-10-24 15:35:29 -0300

    Add files via upload
    agrego el js

---
+++ js/page.js
@@ -0,0 +1,9 @@
+
+
+function nomAp(){
+    var nombre, apellido;
+    nombre = document.getElementById("nombre").value;
+    apellido = document.getElementById("apellido").value;
+
+    alert("Gracias, " + nombre +" " + apellido + " , tu consulta fue enviada con exito.");
+}
