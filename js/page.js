42db8a639b4b93651ae5e46db65fcb6e8c4b243c
nsh <77177369+nsh1z@users.noreply.github.com>
2024-11-12 15:59:22 -0300

    Update page.js

--- js/page.js
+++ js/page.js
@@ -25,13 +25,28 @@ $(document).ready(function() {
         });
     }
 
-    $('#inputBuscar').on('keypress', function(event) {
-        if (event.which === 13) {
-            event.preventDefault();
-            var input = $(this).val().toLowerCase().trim();
-            window.location.href = 'products.html?search=' + encodeURIComponent(input); 
-        }
+  
+    $(document).ready(function() {
+        // Evento para presionar "Enter" en el campo de búsqueda
+        $('#inputBuscar').on('keypress', function(event) {
+            if (event.which === 13) {
+                event.preventDefault();
+                var searchQuery = $(this).val().trim();
+                if (searchQuery) {
+                    window.location.href = 'products.html?search=' + encodeURIComponent(searchQuery);
+                }
+            }
+        });
+    
+        // Evento para hacer clic en el botón de búsqueda (lupa)
+        $('#searchButton').on('click', function() {
+            var searchQuery = $('#inputBuscar').val().trim();
+            if (searchQuery) {
+                window.location.href = 'products.html?search=' + encodeURIComponent(searchQuery);
+            }
+        });
     });
+    
 
     $(".producto img").hover(
         function() {
@@ -58,4 +73,27 @@ document.querySelectorAll('.card').forEach(card => {
     }
         });
     });
+});
+
+
+
+
+document.getElementById('searchButton').addEventListener('click', function() {
+    var searchQuery = document.getElementById('inputBuscar').value.trim();
+    if (searchQuery) {
+        var input = $(this).val().toLowerCase().trim();
+        window.location.href = 'products.html?search=' + encodeURIComponent(input); 
+
+           }
+});
+
+document.getElementById('inputBuscar').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault(); 
+        var searchQuery = document.getElementById('inputBuscar').value.trim();
+        if (searchQuery) {
+            var input = $(this).val().toLowerCase().trim();
+            window.location.href = 'products.html?search=' + encodeURIComponent(input); 
+            }
+    }
 });
